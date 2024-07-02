db881afd2f01d9ba418cfc1a1f0a9ee251cddda8
Sendiko <sarangtawon897@gmail.com>
2024-07-02 11:19:49 +0700

    add navhost, splash screen

--- app/src/main/java/com/mycheva/app/core/MainActivity.kt
+++ app/src/main/java/com/mycheva/app/core/MainActivity.kt
@@ -4,14 +4,12 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.mycheva.app.core.navigation.Screens.SplashScreen
 import com.mycheva.app.core.ui.theme.MyChevaTheme
+import com.mycheva.app.splashscreen.presentation.SplashScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,29 +17,18 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyChevaTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = SplashScreen.name){
+                    /* Tambahkan Screen Composable disini */
+                    composable(
+                        route = SplashScreen.name
+                    ) {
+                        SplashScreen(
+                            onNavigate = { }
+                        )
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyChevaTheme {
-        Greeting("Android")
     }
 }
