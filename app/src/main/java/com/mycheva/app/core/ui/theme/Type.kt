4cca0842f1aab6f88c4d5911615af4f0cd4a4075
Sendiko <sarangtawon897@gmail.com>
2024-07-02 11:08:53 +0700

    import fonst

--- app/src/main/java/com/mycheva/app/core/ui/theme/Type.kt
+++ app/src/main/java/com/mycheva/app/core/ui/theme/Type.kt
@@ -2,9 +2,11 @@ package com.mycheva.app.core.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.mycheva.app.R
 
 // Set of Material typography styles to start with
 val Typography = Typography(
@@ -31,4 +33,25 @@ val Typography = Typography(
         letterSpacing = 0.5.sp
     )
     */
+)
+
+val poppinsFamily = FontFamily(
+    fonts = listOf(
+        Font(
+            resId = R.font.poppins,
+            weight = FontWeight.Normal
+        ),
+        Font(
+            resId = R.font.poppins_medium,
+            weight = FontWeight.Medium
+        ),
+        Font(
+            resId = R.font.poppins_semibold,
+            weight = FontWeight.SemiBold
+        ),
+        Font(
+            resId = R.font.poppins_bold,
+            weight = FontWeight.Bold
+        ),
+    )
 )
