8e80602a9790ae16c5eebc75e6c9874a760275fe
Sendiko <sarangtawon897@gmail.com>
2024-07-06 20:25:23 +0700

    add composables

--- app/src/main/java/com/mycheva/app/core/MainActivity.kt
+++ app/src/main/java/com/mycheva/app/core/MainActivity.kt
@@ -4,11 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.getValue
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.mycheva.app.core.navigation.Screens
 import com.mycheva.app.core.navigation.Screens.SplashScreen
 import com.mycheva.app.core.ui.theme.MyChevaTheme
+import com.mycheva.app.dashboard.presentation.DashboardScreen
+import com.mycheva.app.login.presentation.LoginScreen
+import com.mycheva.app.login.presentation.LoginScreenViewModel
 import com.mycheva.app.splashscreen.presentation.SplashScreen
 
 class MainActivity : ComponentActivity() {
@@ -26,8 +33,26 @@ class MainActivity : ComponentActivity() {
                         route = SplashScreen.name
                     ) {
                         SplashScreen(
-                            onNavigate = { }
+                            onNavigate = {
+                                navController.navigate(Screens.LoginScreen.name)
+                            }
                         )
+                    }
+                    composable(
+                        route = Screens.LoginScreen.name
+                    ) {
+                        val viewModel = viewModel<LoginScreenViewModel>()
+                        val state by viewModel.state.collectAsStateWithLifecycle()
+                        LoginScreen(
+                            state = state,
+                            onEvent = viewModel::onEvent,
+                            onNavigate = { navController.navigate(Screens.DashboardScreen.name) }
+                        )
+                    }
+                    composable(
+                        route = Screens.DashboardScreen.name
+                    ) {
+                        DashboardScreen()
                     }
                 }
             }
