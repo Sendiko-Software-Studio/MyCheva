2596e8a7fe0c8e68d6bb547224f6cef5135d4251
Sendiko <sarangtawon897@gmail.com>
2024-08-03 20:29:56 +0700

    enc(add): forum data class

---
+++ app/src/main/java/com/mycheva/app/forum/main/data/Forum.kt
@@ -0,0 +1,10 @@
+package com.mycheva.app.forum.main.data
+
+data class Forum(
+    val profileUrl: String = "",
+    val name: String = "",
+    val timeStamp: String = "",
+    val content: String = "",
+    val imageUrl: String = "",
+    val totalComment: Int = 0,
+)
