4ba0007fcc551ad0520a3022de7cecab24b6ea00
Sendiko <sarangtawon897@gmail.com>
2024-08-03 20:33:08 +0700

    enc(add): use announcement as parameter

--- app/src/main/java/com/mycheva/app/announcement/presentation/component/AnnouncementCard.kt
+++ app/src/main/java/com/mycheva/app/announcement/presentation/component/AnnouncementCard.kt
@@ -27,16 +27,13 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
+import com.mycheva.app.announcement.data.Announcement
+import kotlinx.serialization.json.JsonNull.content
 
 @Composable
 fun AnnouncementCard(
     modifier: Modifier = Modifier,
-    profileUrl: String,
-    name: String,
-    timeStamp: String,
-    content: String,
-    isBookmarked: Boolean = false,
-    imageUrl: String = "",
+    announcement: Announcement,
     onAddBookMark: () -> Unit,
 ) {
     Column(
@@ -48,7 +45,7 @@ fun AnnouncementCard(
             modifier = Modifier.fillMaxWidth()
         ) {
             AsyncImage(
-                model = profileUrl,
+                model = announcement.profileUrl,
                 modifier = Modifier
                     .size(48.dp)
                     .clip(CircleShape),
@@ -58,19 +55,19 @@ fun AnnouncementCard(
             Spacer(modifier = Modifier.width(8.dp))
             Column {
                 Text(
-                    text = name,
+                    text = announcement.name,
                     fontSize = 20.sp,
                     fontWeight = FontWeight.Bold
                 )
                 Text(
-                    text = timeStamp,
+                    text = announcement.timeStamp,
                     color = Color.Gray
                 )
             }
         }
-        if (imageUrl.isNotBlank()) {
+        if (announcement.imageUrl.isNotBlank()) {
             AsyncImage(
-                model = imageUrl,
+                model = announcement.imageUrl,
                 contentDescription = null,
                 modifier = Modifier.fillMaxWidth(),
                 contentScale = ContentScale.Fit
@@ -82,7 +79,7 @@ fun AnnouncementCard(
             modifier = Modifier.fillMaxWidth(),
         )
         IconButton(onClick = { onAddBookMark() }) {
-            if (isBookmarked) {
+            if (announcement.isBookmarked) {
                 Icon(
                     imageVector = Icons.Default.Bookmark,
                     contentDescription = "bookmarked"
@@ -101,17 +98,19 @@ fun AnnouncementCard(
 @Composable
 private fun AnnouncementCardPrev() {
     Surface {
-        AnnouncementCard(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp),
+        val data = Announcement(
             profileUrl = "https://t3.ftcdn.net/jpg/02/43/12/34/360_F_243123463_zTooub557xEWABDLk0jJklDyLSGl2jrr.jpg",
             name = "Sendiko",
             timeStamp = "1 jam yang lalu",
             content = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ultrices posuere ante id vehicula. Maecenas sed augue fringilla, sodales lacus sed, tempor magna. Aliquam vestibulum tortor convallis nunc fermentum interdum. Duis eleifend, nibh sed egestas mollis, neque tellus efficitur massa, ",
+            isBookmarked = true,
+        )
+        AnnouncementCard(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(16.dp),
             onAddBookMark = { },
-            isBookmarked = true
-
+            announcement = data
         )
     }
 }
