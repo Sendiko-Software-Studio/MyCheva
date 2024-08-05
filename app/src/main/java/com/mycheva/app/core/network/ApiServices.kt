0285c057ec219a716b257ff74ef4d30aaed0afb9
Sendiko <sarangtawon897@gmail.com>
2024-08-05 15:51:29 +0700

    enc(add): userdata endpoint

--- app/src/main/java/com/mycheva/app/core/network/ApiServices.kt
+++ app/src/main/java/com/mycheva/app/core/network/ApiServices.kt
@@ -2,8 +2,11 @@ package com.mycheva.app.core.network
 
 import com.mycheva.app.login.data.LoginRequest
 import com.mycheva.app.login.data.LoginResponse
+import com.mycheva.app.profile.main.data.GetUserResponse
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Header
 import retrofit2.http.POST
 
 interface ApiServices {
@@ -12,5 +15,10 @@ interface ApiServices {
     fun login(
         @Body loginRequest: LoginRequest
     ): Call<LoginResponse>
+
+    @GET("userdata")
+    fun getUser(
+        @Header("Authorization") token: String,
+    ): Call<GetUserResponse>
 
 }
