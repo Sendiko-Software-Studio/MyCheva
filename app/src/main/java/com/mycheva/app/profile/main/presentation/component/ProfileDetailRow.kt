7b78e3a75b762276cdc1e2e9af846398f5b67893
Sendiko <sarangtawon897@gmail.com>
2024-08-06 23:20:17 +0700

    enc(add): color adjustment

--- app/src/main/java/com/mycheva/app/profile/main/presentation/component/ProfileDetailRow.kt
+++ app/src/main/java/com/mycheva/app/profile/main/presentation/component/ProfileDetailRow.kt
@@ -2,15 +2,15 @@ package com.mycheva.app.profile.main.presentation.component
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mycheva.app.core.ui.theme.Neutral600
+import com.mycheva.app.core.ui.theme.Neutral900
 import com.mycheva.app.core.ui.theme.poppinsFamily
 
 @Composable
@@ -18,24 +18,23 @@ fun ProfileDetailRow(label: String, value: String) {
     Column(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(vertical = 1.dp)
     ) {
         Text(
             text = label,
             fontWeight = FontWeight.Normal,
             fontSize = 14.sp,
-            color = Color.Gray,
+            color = Neutral600,
             fontFamily = poppinsFamily
         )
         Text(
             text = value,
             fontWeight = FontWeight.Normal,
             fontSize = 14.sp,
-            color = Color.Black,
+            color = Neutral900,
             fontFamily = poppinsFamily
         )
         HorizontalDivider(
-            color = Color.Gray,
+            color = Neutral600,
             thickness = 1.dp,
             modifier = Modifier.fillMaxWidth()
         )
