fd2110d5ea5449d262153b7ce73ea2b8cf48cdbf
Sendiko <sarangtawon897@gmail.com>
2024-08-06 20:27:34 +0700

    enc(add): change top app bar

--- app/src/main/java/com/mycheva/app/schedule/DetailScheduleScreen.kt
+++ app/src/main/java/com/mycheva/app/schedule/DetailScheduleScreen.kt
@@ -1,13 +1,24 @@
 package com.mycheva.app.schedule
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Divider
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -16,39 +27,24 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mycheva.app.R
+import com.mycheva.app.core.ui.components.CenteredAppBar
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScheduleScreen() {
     Scaffold(
         topBar = {
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color(0xFFFFA500))
-                    .padding(horizontal = 8.dp, vertical = 16.dp)
-            ) {
-                Icon(
-                    painter = painterResource(id = R.drawable.back_logo),
-                    contentDescription = "Logo back",
-                    tint = Color.Unspecified,
-                    modifier = Modifier
-                        .size(40.dp)
-                )
-
-                Text(
-                    text = "Schedule",
-                    fontSize = 23.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    color = Color.White,
-                    modifier = Modifier
-                        .padding(horizontal = 110.dp, vertical = 5.dp)
-                )
-            }
+            CenteredAppBar(
+                title = "Detail Jadwal",
+                navigationIcon = Icons.AutoMirrored.Rounded.ArrowBack,
+                navigationAction = { }
+            )
         },
         content = { paddingValues ->
-            Column(modifier = Modifier
-                .fillMaxSize()
-                .padding(paddingValues)
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(paddingValues)
             ) {
                 EventDetail(paddingValues)
                 DescriptionSection(paddingValues)
@@ -87,9 +83,10 @@ fun EventDetail(paddingValues: PaddingValues) {
 
 @Composable
 fun DescriptionSection(paddingValues: PaddingValues) {
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .padding(16.dp)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
     ) {
         Text(text = "Deskripsi", fontSize = 18.sp, fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.height(8.dp))
@@ -102,9 +99,10 @@ fun DescriptionSection(paddingValues: PaddingValues) {
 
 @Composable
 fun LocationSection(paddingValues: PaddingValues) {
-    Column(modifier = Modifier
-        .fillMaxWidth()
-        .padding(16.dp)
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
     ) {
         Text(text = "Lokasi", fontSize = 18.sp, fontWeight = FontWeight.Bold)
         Spacer(modifier = Modifier.height(8.dp))
--- app/src/main/java/com/mycheva/app/schedule/ScheduleScreen.kt
+++ app/src/main/java/com/mycheva/app/schedule/ScheduleScreen.kt
@@ -1,14 +1,25 @@
 package com.mycheva.app.schedule
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,26 +29,14 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.mycheva.app.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ScheduleScreen() {
     Scaffold(
         topBar = {
-            Box(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(Color(0xFFFFA500))
-                    .padding(horizontal = 8.dp, vertical = 16.dp),
-                contentAlignment = Alignment.Center
-            ) {
-                Text(
-                    text = "Schedule",
-                    fontSize = 23.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    color = Color.White,
-                    modifier = Modifier
-                        .padding(horizontal = 110.dp, vertical = 5.dp)
-                )
-            }
+            CenterAlignedTopAppBar(
+                title = { Text(text = "Jadwal") }
+            )
         },
         content = { paddingValues ->
             Column(modifier = Modifier
