156e766ab7913c8ea37cf6cafcdda0822e69ef20
Sendiko <sarangtawon897@gmail.com>
2024-08-13 14:07:13 +0700

    del(): placeholder class

--- app/src/main/java/com/mycheva/app/forum/comment/data/Comment.kt
+++
@@ -1,8 +0,0 @@
-package com.mycheva.app.forum.comment.data
-
-data class Comment(
-    val profileUrl: String = "",
-    val name: String = "",
-    val timeStamp: String = "",
-    val content: String = "",
-)
