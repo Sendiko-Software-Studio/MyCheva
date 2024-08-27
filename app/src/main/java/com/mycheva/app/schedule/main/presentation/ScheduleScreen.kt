7febfede42a09979281796cd54f01a6959625d8f
Sendiko <sarangtawon897@gmail.com>
2024-08-27 09:39:18 +0700

    enc(add): user interface overhaul

--- app/src/main/java/com/mycheva/app/schedule/main/presentation/ScheduleScreen.kt
+++ app/src/main/java/com/mycheva/app/schedule/main/presentation/ScheduleScreen.kt
@@ -2,36 +2,55 @@ package com.mycheva.app.schedule.main.presentation
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.animation.AnimatedVisibilityScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MediumTopAppBar
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.mycheva.app.core.ui.components.CustomTextField
 import com.mycheva.app.core.ui.components.NotificationBox
-import com.mycheva.app.core.ui.theme.Neutral200
+import com.mycheva.app.core.ui.components.TextFieldType
+import com.mycheva.app.core.ui.components.formatDateString
+import com.mycheva.app.core.ui.theme.Neutral50
+import com.mycheva.app.core.ui.theme.Primary500
 import com.mycheva.app.core.ui.theme.poppinsFamily
-import com.mycheva.app.schedule.main.presentation.component.EventCard
+import com.mycheva.app.dashboard.presentation.component.MeetingCard
 import kotlinx.coroutines.delay
 
 @RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class, ExperimentalSharedTransitionApi::class)
 @Composable
-fun ScheduleScreen(
+fun SharedTransitionScope.ScheduleScreen(
     state: ScheduleScreenState,
+    animatedVisibilityScope: AnimatedVisibilityScope,
     onEvent: (ScheduleScreenEvent) -> Unit,
     onNavigate: (eventId: String) -> Unit
 ) {
@@ -55,18 +74,23 @@ fun ScheduleScreen(
         isErrorNotification = state.isRequestFailed
     ) {
         Scaffold(
+            modifier = Modifier
+                .sharedBounds(
+                    sharedContentState = rememberSharedContentState(key = "schedule"),
+                    animatedVisibilityScope = animatedVisibilityScope
+                ),
             topBar = {
-                MediumTopAppBar(
-                    title = {
-                        Text(
-                            text = "Jadwal",
-                            fontFamily = poppinsFamily,
-                        )
+                CenterAlignedTopAppBar(
+                    navigationIcon = {
+                        IconButton(onClick = { onNavigate("") }) {
+                            Icon(
+                                imageVector = Icons.AutoMirrored.Rounded.ArrowBack,
+                                contentDescription = "back"
+                            )
+                        }
                     },
-                    scrollBehavior = scrollBehavior,
-                    colors = TopAppBarDefaults.largeTopAppBarColors(
-                        scrolledContainerColor = Neutral200
-                    )
+                    title = { Text(text = "Jadwal", fontFamily = poppinsFamily) },
+                    scrollBehavior = scrollBehavior
                 )
             },
             content = { paddingValues ->
@@ -75,9 +99,62 @@ fun ScheduleScreen(
                     verticalArrangement = Arrangement.spacedBy(16.dp),
                     modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection)
                 ) {
+                    item {
+                        CustomTextField(
+                            value = "",
+                            onValueChange = {},
+                            onClearClick = { /*TODO*/ },
+                            type = TextFieldType.White,
+                            modifier = Modifier
+                                .fillMaxWidth()
+                                .padding(horizontal = 16.dp),
+                            label = "Cari..",
+                            leadingIcon = {
+                                Icon(
+                                    imageVector = Icons.Rounded.Search,
+                                    contentDescription = "cari"
+                                )
+                            }
+                        )
+                    }
+                    item {
+                        LazyRow(
+                            contentPadding = PaddingValues(horizontal = 16.dp),
+                            horizontalArrangement = Arrangement.spacedBy(8.dp)
+                        ) {
+                            val dates = state.events.toList().distinctBy {
+                                it.first
+                            }
+                            items(dates) {
+                                Surface(
+                                    shape = RoundedCornerShape(8.dp),
+                                    color = Primary500
+                                ) {
+                                    Column(
+                                        modifier = Modifier.padding(8.dp),
+                                        horizontalAlignment = Alignment.CenterHorizontally
+                                    ) {
+                                        Text(
+                                            text = formatDateString(it.first.toString()).substring(0, 2),
+                                            fontWeight = FontWeight.Bold,
+                                            fontFamily = poppinsFamily,
+                                            color = Neutral50
+                                        )
+                                        Text(
+                                            text = formatDateString(it.first.toString()).substring(2, 6),
+                                            fontWeight = FontWeight.Normal,
+                                            fontFamily = poppinsFamily,
+                                            color = Neutral50,
+                                            fontSize = 16.sp
+                                        )
+                                    }
+                                }
+                            }
+                        }
+                    }
                     items(state.events.entries.toList()) { (date, events) ->
                         Text(
-                            text = date.toString(),
+                            text = formatDateString(date.toString().substring(0, 10)),
                             fontSize = 18.sp,
                             fontWeight = FontWeight.SemiBold,
                             fontFamily = poppinsFamily,
@@ -85,11 +162,12 @@ fun ScheduleScreen(
                         )
                         Spacer(modifier = Modifier.height(8.dp))
                         events.forEach { event ->
-                            EventCard(
+                            MeetingCard(
+                                modifier = Modifier.padding(horizontal = 16.dp),
                                 eventsItem = event,
-                                onClick = { onNavigate(event.id.toString()) },
-                                modifier = Modifier
-                                    .padding(horizontal = 16.dp)
+                                onClick = { eventId ->
+                                    onNavigate(eventId)
+                                }
                             )
                             Spacer(modifier = Modifier.height(8.dp))
                         }
