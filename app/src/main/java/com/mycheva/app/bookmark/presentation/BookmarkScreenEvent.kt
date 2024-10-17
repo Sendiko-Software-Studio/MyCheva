898fb2e3ef0f05a6b8570c4cf45669846363a567
Sendiko <sarangtawon897@gmail.com>
2024-10-17 21:01:48 +0700

    del(): unused code

--- app/src/main/java/com/mycheva/app/bookmark/presentation/BookmarkScreenEvent.kt
+++
@@ -1,7 +0,0 @@
-package com.mycheva.app.bookmark.presentation
-
-import com.mycheva.app.announcement.data.AnnouncementsItem
-
-sealed class BookmarkScreenEvent {
-    data class OnRemoveBookmark(val announcement: AnnouncementsItem): BookmarkScreenEvent()
-}
