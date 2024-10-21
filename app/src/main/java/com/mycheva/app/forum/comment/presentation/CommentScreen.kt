6c7b3ac56952ae38d69c691ebb6ff121f54ad2fa
Sendiko <sarangtawon897@gmail.com>
2024-10-21 14:00:16 +0700

    enc(add): move textfield under the post

--- app/src/main/java/com/mycheva/app/forum/comment/presentation/CommentScreen.kt
+++ app/src/main/java/com/mycheva/app/forum/comment/presentation/CommentScreen.kt
@@ -6,17 +6,16 @@ import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
 import androidx.compose.material.icons.automirrored.rounded.Send
+import androidx.compose.material.icons.filled.Textsms
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
@@ -28,16 +27,13 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import coil.compose.AsyncImage
-import com.mycheva.app.core.network.defaultProfile
+import com.mycheva.app.core.ui.components.CustomTextField
 import com.mycheva.app.core.ui.components.NotificationBox
-import com.mycheva.app.core.ui.components.PlainTextField
+import com.mycheva.app.core.ui.components.TextFieldType
 import com.mycheva.app.core.ui.theme.poppinsFamily
 import com.mycheva.app.forum.comment.presentation.component.CommentCard
 import com.mycheva.app.forum.comment.presentation.component.PostCard
@@ -94,68 +90,79 @@ fun CommentScreen(
                     }
                 )
             },
-            bottomBar = {
-                Row(
-                    modifier = Modifier.padding(16.dp),
-                    verticalAlignment = Alignment.CenterVertically,
-                    horizontalArrangement = Arrangement.spacedBy(8.dp),
-                ) {
-                    AsyncImage(
-                        model = state.userProfileUrl.ifBlank { defaultProfile },
-                        modifier = Modifier
-                            .size(48.dp)
-                            .clip(RoundedCornerShape(8.dp)),
-                        contentDescription = null,
-                        contentScale = ContentScale.Fit
-                    )
-                    PlainTextField(
-                        modifier = Modifier.weight(1f),
-                        value = state.commentText,
-                        onValueChange = {
-                            onEvent(CommentEvent.OnCommentTextChange(it))
-                        },
-                        placeholder = "Berikan komentar anda"
-                    )
-                    IconButton(onClick = { onEvent(CommentEvent.OnPostComment(state.token, state.userId, forumId)) }) {
-                        Icon(
-                            imageVector = Icons.AutoMirrored.Rounded.Send,
-                            contentDescription = "send comment"
-                        )
-                    }
-                }
-            },
             content = { paddingValues ->
-                LazyColumn(
-                    contentPadding = paddingValues,
-                    modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-                    verticalArrangement = Arrangement.spacedBy(8.dp)
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.BottomCenter
                 ) {
-                    item {
-                        Spacer(modifier = Modifier.height(16.dp))
-                    }
-                    item {
-                        AnimatedVisibility(
-                            visible = state.post != null,
-                            enter = fadeIn(),
-                            exit = fadeOut()
-                        ) {
-                            PostCard(forum = state.post!!)
+                    LazyColumn(
+                        contentPadding = paddingValues,
+                        modifier = Modifier
+                            .nestedScroll(scrollBehavior.nestedScrollConnection)
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        item {
+                            AnimatedVisibility(
+                                visible = state.post != null,
+                                enter = fadeIn(),
+                                exit = fadeOut()
+                            ) {
+                                PostCard(forum = state.post!!)
+                            }
                         }
-                    }
-                    item {
-                        Text(
-                            text = "Komentar (${state.totalComment})",
-                            modifier = Modifier.padding(horizontal = 16.dp),
-                            fontWeight = FontWeight.Bold,
-                            fontFamily = poppinsFamily,
-                            fontSize = 14.sp
-                        )
-                    }
-                    items(state.comments) {
-                        CommentCard(
-                            comment = it,
-                            modifier = Modifier.padding(horizontal = 16.dp)
-                        )
+                        item {
+                            Row(
+                                modifier = Modifier.padding(horizontal = 16.dp),
+                                verticalAlignment = Alignment.CenterVertically,
+                                horizontalArrangement = Arrangement.spacedBy(8.dp),
+                            ) {
+                                CustomTextField(
+                                    modifier = Modifier.weight(1f)
+                                        .padding(top = 18.dp),
+                                    value = state.commentText,
+                                    onValueChange = {
+                                        onEvent(CommentEvent.OnCommentTextChange(it))
+                                    },
+                                    label = "Berikan komentar anda",
+                                    onClearClick = { },
+                                    type = TextFieldType.White,
+                                    leadingIcon = {
+                                        Icon(Icons.Default.Textsms, contentDescription = null)
+                                    }
+                                )
+                                IconButton(
+                                    onClick = {
+                                        onEvent(
+                                            CommentEvent.OnPostComment(
+                                                state.token,
+                                                state.userId,
+                                                forumId
+                                            )
+                                        )
+                                    }) {
+                                    Icon(
+                                        imageVector = Icons.AutoMirrored.Rounded.Send,
+                                        contentDescription = "send comment"
+                                    )
+                                }
+                            }
+                        }
+                        item {
+                            Text(
+                                text = "Komentar (${state.totalComment})",
+                                modifier = Modifier.padding(horizontal = 16.dp),
+                                fontWeight = FontWeight.Bold,
+                                fontFamily = poppinsFamily,
+                                fontSize = 14.sp
+                            )
+                        }
+                        items(state.comments) {
+                            CommentCard(
+                                comment = it,
+                                modifier = Modifier.padding(horizontal = 16.dp)
+                            )
+                        }
                     }
                 }
             }
