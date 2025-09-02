0ebd4ea4f32f6e9177e3115cd1fe4930a7754278
Sendiko <rizkysendiko7@gmail.com>
2025-09-02 10:51:13 +0700

    feat: implement dependency injection using Koin
    This commit introduces Koin for dependency injection and sets up modules for:
    - Network services (Ktor client, OkHttp engine, ApiServices)
    - Database (Room, BookmarkDao)
    - Preferences (Datastore)
    - Repositories (Announcement, Attendance, Bookmark, Profile)
    - ViewModels (Announcement, Attendance, Bookmark, Profile)

--- app/src/main/java/com/mycheva/app/core/di/KoinModules.kt
+++ app/src/main/java/com/mycheva/app/core/di/KoinModules.kt
@@ -1,19 +1,42 @@
 package com.mycheva.app.core.di
 
+import com.mycheva.app.announcement.data.AnnouncementRepositoryImpl
+import com.mycheva.app.announcement.presentation.AnnouncementViewModel
+import com.mycheva.app.attendance.data.AttendanceRepositoryImpl
+import com.mycheva.app.attendance.presentation.AttendanceViewModel
+import com.mycheva.app.bookmark.data.BookmarkRepositoryImpl
+import com.mycheva.app.bookmark.presentation.BookmarkViewModel
 import com.mycheva.app.core.database.RoomFactory
+import com.mycheva.app.core.network.ApiServices
 import com.mycheva.app.core.network.HttpClientFactory
 import com.mycheva.app.core.network.KtorClient
 import com.mycheva.app.core.preferences.DatastoreFactory
+import com.mycheva.app.profile.main.data.ProfileRepositoryImpl
+import com.mycheva.app.profile.main.presentation.ProfileViewModel
 import io.ktor.client.engine.HttpClientEngine
 import io.ktor.client.engine.okhttp.OkHttp
 import org.koin.android.ext.koin.androidContext
+import org.koin.core.module.dsl.singleOf
+import org.koin.core.scope.get
+import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val koinModules = module {
     single<HttpClientEngine> { OkHttp.create() }
-
     single { HttpClientFactory.create(get()) }
     single { RoomFactory.create(androidContext()) }
+    single { RoomFactory.create(androidContext()).bookmarkDao }
     single { DatastoreFactory.create(androidContext()) }
     single { KtorClient(get()) }
+
+    singleOf(::KtorClient).bind<ApiServices>()
+    singleOf(::AnnouncementRepositoryImpl)
+    singleOf(::AttendanceRepositoryImpl)
+    singleOf(::BookmarkRepositoryImpl)
+    singleOf(::ProfileRepositoryImpl)
+
+    factory { AnnouncementViewModel(get()) }
+    factory { AttendanceViewModel(get()) }
+    factory { BookmarkViewModel(get()) }
+    factory { ProfileViewModel(get()) }
 }
