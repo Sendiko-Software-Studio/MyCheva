35b02ddf5a258dd44bc0c28ac475c3bc354d7341
Sendiko <rizkysendiko7@gmail.com>
2025-09-02 17:53:36 +0700

    refactor: Remove Comment feature files
    This commit removes the following files related to the Comment feature:
    - `app/src/main/java/com/mycheva/app/forum/comment/presentation/CommentEvent.kt`
    - `app/src/main/java/com/mycheva/app/forum/comment/domain/CommentRepository.kt`
    - `app/src/main/java/com/mycheva/app/forum/comment/domain/CommentRepositoryImpl.kt`
    - `app/src/main/java/com/mycheva/app/forum/comment/presentation/CommentScreenState.kt`
    - `app/src/main/java/com/mycheva/app/forum/comment/presentation/CommentUi.kt`

--- app/src/main/java/com/mycheva/app/forum/comment/presentation/CommentEvent.kt
+++
@@ -1,8 +0,0 @@
-package com.mycheva.app.forum.comment.presentation
-
-sealed class CommentEvent {
-    data class OnCommentTextChange(val value: String) : CommentEvent()
-    data object OnClearState: CommentEvent()
-    data class OnLoadData(val token: String, val forumId: String): CommentEvent()
-    data class OnPostComment(val token: String, val userId: String, val forumId: String): CommentEvent()
-}
